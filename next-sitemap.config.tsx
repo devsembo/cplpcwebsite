b1a4f9fc795b6479eda56cc525e05a1d84847031
Anderson Pedro <anderson.ap01@icloud.com>
2025-07-23 14:56:16 +0100

    feat: site map

---
+++ next-sitemap.config.tsx
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://cplpconnect.pt',
+    generateRobotsTxt: true, // Gera também o robots.txt
+    sitemapSize: 5000,
+};
